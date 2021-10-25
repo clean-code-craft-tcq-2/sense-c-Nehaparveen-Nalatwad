35315e529d25d4bd1310a0cfff80071894672e55
Nehaparveen-Nalatwad <91203086+Nehaparveen-Nalatwad@users.noreply.github.com>
2021-10-25 20:07:55 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -9,12 +9,9 @@
 TEST_CASE("reports average, minimum and maximum") {
     float numberset[] = {1.5, 8.9, 3.2, 4.5};
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    //float avg = 0.0;
-    //float min = 0.0;
-    //float max = 0.0;
-    int main(numberset, setlength)
+    int compute_statistic(numberset, setlength);
     float epsilon = 0.001;
-    REQUIRE(abs(s.average - 4.525) < epsilon);
+    REQUIRE(abs(average - 4.525) < epsilon);
     //REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     //REQUIRE(abs(computedStats.min - 1.5) < epsilon);
 }
